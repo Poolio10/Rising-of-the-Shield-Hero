ef03f995a3011a4e511a6475266c078dd663ce7e
Poolio10 <57581481+Poolio10@users.noreply.github.com>
2019-11-11 10:36:29 -0600

    Fixing an error on my part, remaking strings public

--- src/main/java/poolio10/risingoftheshieldhero/RisingoftheShieldHero.java
+++ src/main/java/poolio10/risingoftheshieldhero/RisingoftheShieldHero.java
@@ -13,11 +13,11 @@ import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 public class RisingoftheShieldHero {
 
     public static final String MODID = "risingoftheshieldhero";
-    static final String NAME = "Rising of the Shield Hero";
-    static final String VERSION = "1.0.0";
-    static final String MC_VERSION = "[1.12.2]";
+    public static final String NAME = "Rising of the Shield Hero";
+    public static final String VERSION = "1.0.0";
+    public static final String MC_VERSION = "[1.12.2]";
 
-    private static final Logger LOGGER = LogManager.getLogger(RisingoftheShieldHero.MODID);
+    public static final Logger LOGGER = LogManager.getLogger(RisingoftheShieldHero.MODID);
 
     @EventHandler
     public void preInit(FMLPreInitializationEvent event) {
