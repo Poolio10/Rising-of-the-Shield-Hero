4fbdfbc1fbb17df1666f30d46a94ce9f23473ee6
Poolio10 <57581481+Poolio10@users.noreply.github.com>
2019-11-11 23:57:14 -0600

    Fixing stupid things on my part

--- src/main/java/poolio10/risingoftheshieldhero/RisingoftheShieldHero.java
+++ src/main/java/poolio10/risingoftheshieldhero/RisingoftheShieldHero.java
@@ -13,7 +13,7 @@ import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 public class RisingoftheShieldHero {
 
     public static final String MODID = "risingoftheshieldhero";
-    public static final String NAME = "Rising of the Shield Hero";
+    public static final String NAME = "Rising of the Shield Hero Mod";
     public static final String VERSION = "1.0.0";
     public static final String MC_VERSION = "[1.12.2]";
 
